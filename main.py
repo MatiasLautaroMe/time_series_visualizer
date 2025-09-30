917f67ab8453a2d03ee17fc692ed4155429774af
MatiasLautaroMe <matias.lautaro@live.com.ar>
2025-09-30 19:57:28 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Probar las funciones
+if __name__ == "__main__":
+    draw_line_plot()
+    draw_bar_plot()
+    draw_box_plot()
+    print("¡Todos los gráficos generados exitosamente!")
